f1c718e5da8dfddf0e7a1a831c54bc91d4893c76
marjunhingpit21-svg <marjunhingpit21@gmail.com>
2025-11-26 10:55:15 +0800

    hffn

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     } else {
                         header('Location: index.php');
                     }
-                    exit();
+                    exit();   
                 } else {
                     $error = 'Invalid email or password.';
                 }
