feecf4aa81fccabada1c0dd84ea95bcd56c828cd
marjunhingpit21-svg <marjunhingpit21@gmail.com>
2025-11-28 15:54:05 +0800

    3:53pm

--- database/db.php
+++ database/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","trendywear_store") or die("nope");
+$conn = mysqli_connect("localhost","root","","store2") or die("nope");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
